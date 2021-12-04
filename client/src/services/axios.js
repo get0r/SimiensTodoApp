96647b2132fc3abfac71fe82e03cfa78816cb29e
jahsirtg@protonmail.com <jahsirtg@protonmail.com>
2021-12-04 16:59:26 +0300

    Add environment vars for base api url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3048/api/v1';
+
 export const api = axios.create({
-    baseURL: '',
+    baseURL: baseURL,
     headers: {
         "Content-type": "application/json"
     },
